cbd1795cd409f333771084d6d81b307ff837a9dd
Couesnon Mikaël <mikael.couesnon@gmail.com>
2020-04-08 11:05:46 +0200

    clean

--- src/lib/Filter.js
+++ src/lib/Filter.js
@@ -2,32 +2,6 @@ import Utilities from './Utilities';
 
 export default class Filter {
 
-    createBlacklist(type) {
-
-        let blacklist = [];
-
-        let confCats = window.cookieConsent.config.categories;
-        let confServices = window.cookieConsent.config.services;
-
-        for (let serviceKey in confServices) {
-            if (confServices.hasOwnProperty(serviceKey)) {
-                let confService = confServices[serviceKey];
-                if (confService.type === type && confCats[confService.category].needed === false && confCats[confService.category].wanted === false) {
-                    let searchType = Utilities.objectType(confService.search);
-                    if (searchType === 'String') {
-                        blacklist.push(confService.search);
-                    } else if (searchType === 'Array') {
-                        for (let i = 0; i < confService.search.length; i++) {
-                            blacklist.push(confService.search[i]);
-                        }
-                    }
-                }
-            }
-        }
-
-        return blacklist;
-    }
-
     createSearchCatsMap(type) {
         let searchCats = {};
         let confServices = window.cookieConsent.config.services;
